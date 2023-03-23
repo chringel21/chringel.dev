f899d4148fb66994384b0bf3bfd3f02503edb772
Christian Engel <ch-engel@posteo.de>
2023-03-23 13:43:17 +0100

    New permalink for notes

---
+++ content/notes/notes.11tydata.js
@@ -0,0 +1,5 @@
+module.exports = {
+  type: "note",
+  permalink:
+    "/notes/{{ page.date | permalink_year }}/{{ page.date | permalink_month }}/{{ page.date | permalink_day }}/{{ page.fileSlug }}/",
+};
