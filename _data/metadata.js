13ac184de81763596f4ffb9ca6d62fe5066495c5
Christian Engel <ch-engel@posteo.de>
2023-03-24 09:24:23 +0100

    Add metadata config

---
+++ _data/metadata.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: "chringel.dev",
+  url: "https://chringel.dev/",
+  language: "en",
+  description:
+    "My personal blog about coding, spatial stuff and everything else",
+  author: {
+    name: "Christian Engel",
+    email: "ch-engel@posteo.de",
+    url: "https://chringel.dev/about/",
+  },
+};
