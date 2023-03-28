042a7ee72e4ed904bac8f846ffc013db03827a8c
Christian Engel <ch-engel@posteo.de>
2023-03-28 08:49:52 +0200

    Add notes collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -24,6 +24,45 @@ const blogCollection = defineCollection({
   }),
 });
 
+const notesCollection = defineCollection({
+  schema: z.object({
+    author: z.string(),
+    type: z.string(),
+    date: z.date(),
+    categories: z.array(z.string()),
+    tags: z.array(z.string()),
+    description: z.string(),
+    bridgy: z.array(z.string()).optional(),
+    syndication: z
+      .object({
+        twitter: z.string().optional(),
+        mastodon: z.string().optional(),
+      })
+      .optional(),
+    reply: z
+      .object({
+        url: z.string().optional(),
+      })
+      .optional(),
+    like: z
+      .object({
+        url: z.string().optional(),
+      })
+      .optional(),
+    repost: z
+      .object({
+        url: z.string().optional(),
+      })
+      .optional(),
+    quote: z
+      .object({
+        url: z.string().optional(),
+      })
+      .optional(),
+  }),
+});
+
 export const collections = {
   blog: blogCollection,
+  notes: notesCollection,
 };
