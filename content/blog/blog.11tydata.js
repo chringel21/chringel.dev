63ef46fe9f2055c377fc32004ef92ad4ddb022fb
Christian Engel <ch-engel@posteo.de>
2023-04-06 22:33:26 +0200

    Add cover image path to frontmatter

--- content/blog/blog.11tydata.js
+++ content/blog/blog.11tydata.js
@@ -3,4 +3,9 @@ module.exports = {
   layout: "layouts/post.webc",
   permalink:
     "/blog/{{ page.date | permalink_year }}/{{ page.date | permalink_month }}/{{ page.fileSlug }}/",
+  eleventyComputed: {
+    frontmatter: (data) => data,
+    coverImage:
+      "{% if frontmatter.cover.src and 'http' in frontmatter.cover.src %}{{ frontmatter.cover.src }}{% else %}./content{{ page.filePathStem | replace('index', frontmatter.cover.src) }}{% endif %}",
+  },
 };
