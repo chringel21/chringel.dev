6b0fb6476877cd6d8171b78afd3561a280b440ec
Christian Engel <ch-engel@posteo.de>
2023-05-11 08:50:31 +0200

    Use correct blog post slug

--- content/blog/blog.11tydata.js
+++ content/blog/blog.11tydata.js
@@ -1,13 +1,19 @@
+const slugify = require("slugify");
+
 module.exports = {
   tags: ["posts"],
   layout: "layouts/post.webc",
   permalink: (data) => {
     const year = String(data.page.date.getFullYear());
     const month = String(data.page.date.getMonth() + 1).padStart(2, "0");
+    const slugifiedTitle = slugify(data.title, {
+      lower: true,
+      strict: true,
+    });
     if (data.page.date > new Date("2023-02-21T12:00:00")) {
-      return `/blog/${year}/${month}/${data.page.fileSlug}/`;
+      return `/blog/${year}/${month}/${slugifiedTitle}/`;
     }
-    return `/${year}/${month}/${data.page.fileSlug}/`;
+    return `/${year}/${month}/${slugifiedTitle}/`;
   },
   eleventyComputed: {
     frontmatter: (data) => data,
