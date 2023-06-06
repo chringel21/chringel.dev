b19e4e1378f43069f0dc984b0be906c72d61646c
Christian Engel <ch-engel@posteo.de>
2023-06-06 14:20:55 +0200

    Update notes frontmatter

--- content/notes/notes.11tydata.js
+++ content/notes/notes.11tydata.js
@@ -4,10 +4,13 @@ module.exports = {
   permalink: (data) => {
     const year = String(data.page.date.getFullYear());
     const month = String(data.page.date.getMonth() + 1).padStart(2, "0");
-    const day = String(data.page.date.getDay()).padStart(2, "0");
+    const day = String(data.page.date.getDate()).padStart(2, "0");
     if (data.page.date > new Date("2023-03-15T12:00:00")) {
       return `/notes/${year}/${month}/${day}/${data.page.fileSlug}/`;
     }
     return `/${year}/${month}/${day}/${data.page.fileSlug}/`;
+  },
+  eleventyComputed: {
+    frontmatter: (data) => data,
   },
 };
