1b3b46461489e75a8570c095c193d63953c3c80c
Christian Engel <ch-engel@posteo.de>
2023-06-09 13:58:34 +0200

    Add categories

--- _includes/utils/collections.js
+++ _includes/utils/collections.js
@@ -1,3 +1,6 @@
+const { getCategoryKeys } = require("./functions.js");
+const slugify = require("slugify");
+
 module.exports = {
   allReverse: (collectionApi) => {
     return collectionApi.getAllSorted().reverse();
@@ -28,5 +31,38 @@ module.exports = {
         postsBySeries[series].push(item);
       });
     return postsBySeries;
+  },
+  categories: (collectionApi) => {
+    const options = { categoryVar: "categories" };
+    const posts = collectionApi.getFilteredByTag("posts");
+    let tagArray = getCategoryKeys(posts, options);
+
+    const categoriesWithPosts = tagArray.map((category) => {
+      let filteredPosts = posts
+        .filter((post) => {
+          if (!post.data[options.categoryVar]) return false;
+          return post.data[options.categoryVar].includes(category);
+        })
+        .flat();
+      console.log(
+        slugify(category, {
+          lower: true,
+          strict: true,
+        })
+      );
+      return {
+        title: category,
+        slug: slugify(category, {
+          lower: true,
+          strict: true,
+        }),
+        posts: [...filteredPosts],
+      };
+    });
+    console.log(
+      `\x1b[32m[Dynamic Categories] Created Collection ${options.categoryVar} with ${categoriesWithPosts.length} items`,
+      "\x1b[0m"
+    );
+    return categoriesWithPosts;
   },
 };
