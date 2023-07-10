4adb77609725b7cd405e9716fa911c7efd71b5d9
Christian Engel <ch-engel@posteo.de>
2023-07-10 15:56:14 +0200

    Add globals for env and umami

---
+++ _data/build.js
@@ -0,0 +1,7 @@
+module.exports = {
+  env: process.env.ELEVENTY_ENV,
+  umami: {
+    websiteId: "59e2e93a-9a05-4d4c-b671-33eaec1b46f7",
+    jsLocation: "https://analytics.chringel.dev/umami.js",
+  },
+};
