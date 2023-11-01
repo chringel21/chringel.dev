11362238bfb3765c489e6b48ae87dc4a91a55050
Christian Engel <christian.e@schindelhauerbikes.de>
2023-11-01 11:08:56 +0100

    Update umami script location

--- _data/build.js
+++ _data/build.js
@@ -2,6 +2,6 @@ module.exports = {
   env: process.env.ELEVENTY_ENV,
   umami: {
     websiteId: "59e2e93a-9a05-4d4c-b671-33eaec1b46f7",
-    jsLocation: "https://analytics.chringel.dev/umami.js",
+    jsLocation: "https://analytics.chringel.dev/script.js",
   },
 };
