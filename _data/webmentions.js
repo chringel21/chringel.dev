815e2e9bf3439dbb6a14513ddb565801c499721f
Christian Engel <christian.e@schindelhauerbikes.de>
2023-11-14 08:05:33 +0100

    Add netlify cache plugin

--- _data/webmentions.js
+++ _data/webmentions.js
@@ -7,13 +7,22 @@ require("dotenv").config();
 const API = "https://jam.chringel.dev/webmention/chringel.dev/";
 const TOKEN = process.env.JAM_TOKEN;
 const DOMAIN = metadata.url;
+const URL = `${API}${TOKEN}`;
 
 module.exports = async () => {
-  const json = await EleventyFetch(`${API}${TOKEN}`, {
+  if (!TOKEN) {
+    console.warn(
+      "Unable to fetch webmentions: no access token specified in environment."
+    );
+    return false;
+  }
+  console.log(`Fetching webmentions from ${URL}`);
+  const json = await EleventyFetch(URL, {
     duration: "1d",
     type: "json",
   });
 
+  console.log(`${json.json.length} webmentions fetched from ${URL}`);
   const webmentions = json.json.map((mention) => {
     mention.relativeTarget = mention.target.substring(
       mention.target.indexOf(DOMAIN) + DOMAIN.length,
